4123094cd4a750c0aa2e26521e3519ca8af0ece9
Kaktus <ppsspplouco@gmail.com>
2025-03-29 01:43:54 -0300

    Quadro de Tarefas
    Organizador e gestor de tarefas, o quadro de tarefas é uma ferramenta essencial para qualquer profissional ou estudante que busca aumentar sua produtividade e eficiência. Com ele, é possível visualizar e priorizar tarefas, definir prazos e responsáveis, e acompanhar o progresso de cada atividade. Além disso, o quadro de tarefas ajuda a reduzir o estresse e a ansiedade, permitindo que você se concentre em uma tarefa de cada vez e alcance seus objetivos de forma mais eficaz."

---
+++ script.js
@@ -0,0 +1,81 @@
+const columns = document.querySelectorAll(".column__cards");
+
+let draggedCard;
+
+const dragStart = (event) => {
+    draggedCard = event.target;
+    event.dataTransfer.effectAllowed = "move";
+};
+
+const dragOver = (event) => {
+    event.preventDefault();
+};
+
+const dragEnter = ({ target }) => {
+    if (target.classList.contains("column__cards")) {
+        target.classList.add("column--highlight");
+    }
+};
+
+const dragLeave = ({ target }) => {
+    target.classList.remove("column--highlight");
+};
+
+const drop = ({ target }) => {
+    if (target.classList.contains("column__cards")) {
+        target.classList.remove("column--highlight");
+        target.append(draggedCard);
+    }
+};
+
+const createCard = ({ target }) => {
+    if (!target.classList.contains("column__cards")) return;
+
+    const card = document.createElement("section");
+
+    card.className = "card";
+    card.draggable = "true";
+    card.contentEditable = "true";
+
+    card.addEventListener("focusout", () => {
+        card.contentEditable = "false";
+        if (!card.textContent) card.remove();
+    });
+
+    card.addEventListener("dragstart", dragStart);
+
+    target.append(card);
+    card.focus();
+};
+
+columns.forEach((column) => {
+    column.addEventListener("dragover", dragOver);
+    column.addEventListener("dragenter", dragEnter);
+    column.addEventListener("dragleave", dragLeave);
+    column.addEventListener("drop", drop);
+    column.addEventListener("dblclick", createCard);
+});
+
+
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+
+})
+
